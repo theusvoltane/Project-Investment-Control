b33fe49cb65ac0febfe2104ca80bd98a35f28809
Matheus Voltane Mendonça <65687322+theusvoltane@users.noreply.github.com>
2020-07-08 18:02:18 -0300

    Update CadastroView.java
    Tela de cadastro com o primeiro JLabel e o primeiro JTextFiled adicionado.

--- src/main/java/com/matheusvoltane/view/CadastroView.java
+++ src/main/java/com/matheusvoltane/view/CadastroView.java
@@ -1,19 +1,48 @@
 package com.matheusvoltane.view;
 
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class CadastroView extends JFrame {
 	private	static final long serialVersionUID = 1L;
 	
 	private JLabel cadastroLabel;
+	private JButton cadastroButton;
+	private JTextField cadastroTextField;
+	
 	
 	public CadastroView () {
-		
+		initComponents();
+		initView();
+		init();
+		this.setVisible(true);
 	}
 	
 	private void init( ) {
-		
+		this.setTitle("Cadastro");
+		this.setSize(380, 200);
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	
+	private void initView() {
+		JPanel painel = new JPanel();
+		painel.setLayout(new GridLayout(0, 2));
+		
+		painel.add(cadastroLabel);
+		painel.add(cadastroTextField);
+		
+		this.add(painel);
+	}
+	
+	private void initComponents() {
+		cadastroLabel = new JLabel("Valor Investido");
+		cadastroTextField = new JTextField(" ");
+	}
 }
