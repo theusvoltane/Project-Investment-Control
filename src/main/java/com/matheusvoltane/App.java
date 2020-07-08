46a52d739062fb8a417713a660339c06432bf4f7
Matheus Voltane Mendonça <65687322+theusvoltane@users.noreply.github.com>
2020-07-08 18:01:04 -0300

    Update App.java
    @SuppressWarnings adicionado.

--- src/main/java/com/matheusvoltane/App.java
+++ src/main/java/com/matheusvoltane/App.java
@@ -3,7 +3,8 @@ package com.matheusvoltane;
 import com.matheusvoltane.view.CadastroView;
 
 public class App {
-    public static void main( String[] args ){
+    @SuppressWarnings("unused")
+	public static void main( String[] args ){
         CadastroView tela = new CadastroView();
     }
 }
