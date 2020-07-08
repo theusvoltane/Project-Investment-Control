b4401bd448feddb3c43d6621b77c8d756136e187
Matheus Voltane Mendonça <65687322+theusvoltane@users.noreply.github.com>
2020-07-08 17:39:21 -0300

    Create CadastroView.java
    inicio da criação da tela, com JFrame.

---
+++ src/main/java/com/matheusvoltane/view/CadastroView.java
@@ -0,0 +1,19 @@
+package com.matheusvoltane.view;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class CadastroView extends JFrame {
+	private	static final long serialVersionUID = 1L;
+	
+	private JLabel cadastroLabel;
+	
+	public CadastroView () {
+		
+	}
+	
+	private void init( ) {
+		
+	}
+	
+}
