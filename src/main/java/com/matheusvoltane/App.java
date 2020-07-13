51ef11f631886f11b6faac8ead525d2c37fdd60d
Matheus Voltane Mendonça <65687322+theusvoltane@users.noreply.github.com>
2020-07-13 13:26:53 -0300

    Update App.java
    Executador do aplicativo.

--- src/main/java/com/matheusvoltane/App.java
+++ src/main/java/com/matheusvoltane/App.java
@@ -1,10 +1,31 @@
 package com.matheusvoltane;
 
-import com.matheusvoltane.view.CadastroView;
+import java.util.Scanner;
+
+import com.matheusvoltane.view.CriarAplicacaoConsole;
+import com.matheusvoltane.view.RemoverAplicacao;
 
 public class App {
-    @SuppressWarnings("unused")
-	public static void main( String[] args ){
-        CadastroView tela = new CadastroView();
-    }
+	@SuppressWarnings( "resource" )
+	public static void main(String[] args) {
+		System.out.println("*** APLICAÇÔES ***");
+		System.out.println("\n***Digite a opção que deseja\n 1 - criar\n 2 - remover");
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Digite: ");
+		int opcao = Integer.parseInt(scan.nextLine());
+
+		if (opcao == 1) {
+			CriarAplicacaoConsole  criar = new CriarAplicacaoConsole();
+			
+			criar.criarAplicacao();
+		} else {
+			RemoverAplicacao remover = new RemoverAplicacao();
+			
+			remover.removerAplicacao();
+
+		}
+
+	}
 }
